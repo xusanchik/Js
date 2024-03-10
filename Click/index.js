f2ca9a12512b3447987ff7eeb9dee473e68a8925
Husanboy <samadovxusan9013@gmail.com>
2024-03-10 15:26:06 +0500

    add mouse

--- Click/index.js
+++ Click/index.js
@@ -1,9 +1,36 @@
 
-let btn = document.querySelector("#Btn");
+// let btn = document.querySelector("#Btn");
 
 
-// Click yani bir mata bosilganda ishilidi 
-btn.addEventListener("click", () => {
-  // alert ("Salom");
-})
+// // Click yani bir mata bosilganda ishilidi\
+// btn.addEventListener("click", () => {
+//   // alert ("Salom");
+// })
 
+// //  dbclick => ikki marta bosganda ishlidi
+// btn.addEventListener("dblclick", () => {
+//   // alert ("SalomDbl");
+// })
+
+// // contextmenu => sichqonchani ong tarafi bosilganda ishlidi  ishlab menu chiqadi
+// btn.addEventListener("contextmenu", () => {
+//   alert ("Salom menu");
+// })
+
+let div = document.querySelector("div");
+
+div.addEventListener("mouseenter",() => {
+    div.style.backgroundColor = "blue";
+  console.log("mishka kirdi ")
+});
+
+div.addEventListener("mouseleave",() => {
+  console.log("mishka chiqdi ")
+});
+
+div.addEventListener("mouseover",() => {
+  console.log("over")
+});
+div.addEventListener("mouseout",() => {
+  console.log("out")
+});
