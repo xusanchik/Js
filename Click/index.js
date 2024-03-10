acacb81032e9e78835217c177d96d41133c5dc54
Husanboy <samadovxusan9013@gmail.com>
2024-03-10 15:25:02 +0500

    add Mouse

--- Click/index.js
+++ Click/index.js
@@ -7,12 +7,3 @@ btn.addEventListener("click", () => {
   // alert ("Salom");
 })
 
-//  dbclick => ikki marta bosganda ishlidi
-btn.addEventListener("dblclick", () => {
-  // alert ("SalomDbl");
-})
-
-// contextmenu => sichqonchani ong tarafi bosilganda ishlidi  ishlab menu chiqadi
-btn.addEventListener("contextmenu", () => {
-  alert ("Salom menu");
-})
